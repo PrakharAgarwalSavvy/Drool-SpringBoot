2e61ed779309f23ed7f411c05551c9eaf812b3b4
Prakhar_Agarwal <Prakhar_Agarwal@EPINHYDW01F9.princeton.epam.com>
2021-05-15 19:47:23 +0530

    drool-decision-table

---
+++ drool-decision-table-poc/src/main/java/com/learning/DroolPocApplication.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolPocApplication.class, args);
+	}
+
+}
